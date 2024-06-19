db6c5f9cf270a49360fd7df154160e5ae4f06a08
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-19 15:32:37 +0200

    feat: ProjectTableSeeder

--- database/migrations/2024_06_19_125437_create_project_table.php
+++ database/migrations/2024_06_19_125437_create_project_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('project', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('title')->unique();
             $table->text('content')->nullable();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('project');
+        Schema::dropIfExists('projects');
     }
 };
