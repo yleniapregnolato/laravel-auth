822a7f871e52474ba8fbd200b671fc019d813c01
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-19 15:42:25 +0200

    feat: route resouce in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -25,6 +26,7 @@ Route::middleware('auth')
     ->name('admin.') // inizio di ogni nome delle rotte del gruppo
     ->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('project', ProjectController::class);
     });
 
 require __DIR__ . '/auth.php';
