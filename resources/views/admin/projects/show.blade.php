f0e0c1b72d2e49a75200d569c1cdf6b2c885ff6d
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-20 16:18:52 +0200

    fix: show.blade.php

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,7 +2,11 @@
 
 @section('content')
     <h1 class="mt-4">DETTAGLI DEL PROGETTO:</h1>
-
+    
+    <div>
+        <h3>Titolo:</h3>
+        <p class="fw-bold">{{ $project->title }}</p>
+    </div>
     <div>
         <h3>Contenuto:</h3>
         <p>{{ $project->content }}</p>
